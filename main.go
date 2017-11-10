1915dd099bf21c10e826eb625206fbc3040949e9
Derek Chan <derekchan1994@gmail.com>
2017-11-10 00:25:46 -0800

    boilerplate done, except still cannot control iframe video

--- main.go
+++ main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
+
+type video struct {
+	ID   string
+	Type string
+}
+
+const (
+	templatesIndex = "templates/index.html"
+)
+
+func templateHandler(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			v := video{"0B7ZcMAAjRGDleXV1TWFJTmJFSHc", "video/mp4"}
+			t, err := template.ParseFiles(templatesIndex)
+			if err != nil {
+				panic(err)
+			}
+			err = t.Execute(w, v)
+			if err != nil {
+				panic(err)
+			}
+			return
+		} else if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	}
+}
 
 func main() {
 	port := flag.String("p", "8080", "port to serve on")
-	dir := flag.String("d", "./public", "directory to host")
 	flag.Parse()
 
-	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	http.HandleFunc("/", templateHandler(http.FileServer(http.Dir("./public"))))
 	log.Printf("Serving on port %s", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
