b428f689e7fb44630c5e67cd0ad688c3a7661e05
Derek Chan <derekchan1994@gmail.com>
2017-12-03 15:29:36 -0800

    local videos working, play/pause

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
-$(document).ready(function () {
+(function () {
     "use strict";
-    console.log($("#video"));
-    console.log("Hi");
-});
+    var playFile = function () {
+        var file = this.files[0],
+            fileURL = URL.createObjectURL(file);
+        $("#videoplayer")[0].src = fileURL;
+        console.log("Should be playing " + fileURL);
+    };
+
+    $("#input").on("change", playFile);
+
+    // Controls
+    var videoplayerdom = $("#videoplayer")[0];
+    $("#controls-play").on("click", function () {
+        if (videoplayerdom.paused) {
+            videoplayerdom.play();
+        } else {
+            videoplayerdom.pause();
+        }
+    });
+
+    $(document).ready(function () {
+        console.log("Loaded");
+    });
+}());
