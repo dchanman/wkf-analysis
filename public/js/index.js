8460b92fae75fd9c7c2022af58e471ae6daed357
Derek Chan <derekchan1994@gmail.com>
2017-12-03 16:24:12 -0800

    better progressbar

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,8 @@
         console.log("Ready");
         // Controls
         var videoplayer = $("#videoplayer"),
-            videoplayerdom = videoplayer[0];
+            videoplayerdom = videoplayer[0],
+            progressbarDrag = false;
         $("#controls-play").on("click", function () {
             if (videoplayerdom.paused) {
                 videoplayerdom.play();
@@ -21,12 +22,40 @@
                 videoplayerdom.pause();
             }
         });
+        function updateTimebarPosition(percentage) {
+            $(".controls-progressbar-progressbar-timebar").css("width", percentage + "%");
+        }
+        function setVideoplayerPosition(e) {
+            var progressbar = $(".controls-progressbar-progressbar"),
+                maxTime = videoplayerdom.duration,
+                position = e.pageX - progressbar.offset().left,
+                percentage = position / progressbar.width() * 100;
+            percentage = percentage > 100 ? 100 : percentage;
+            percentage = percentage < 0 ? 0 : percentage;
+            videoplayerdom.currentTime = maxTime * percentage / 100;
+            updateTimebarPosition(percentage);
+        }
+        $(".controls-progressbar-progressbar").on("mousedown", function (e) {
+            progressbarDrag = true;
+            setVideoplayerPosition(e);
+        });
+        $(document).on("mouseup", function (e) {
+            if (progressbarDrag) {
+                progressbarDrag = false;
+                setVideoplayerPosition(e);
+            }
+        });
+        $(document).on("mousemove", function (e) {
+            if (progressbarDrag) {
+                setVideoplayerPosition(e);
+            }
+        });
         videoplayer.on("timeupdate", function () {
             var currentPosition = videoplayerdom.currentTime,
                 maxTime = videoplayerdom.duration,
                 percentage = currentPosition / maxTime * 100;
             console.log(percentage + "%");
-            $(".controls-progressbar-progressbar-timebar").css("width", percentage + "%");
+            updateTimebarPosition(percentage);
         });
     });
 }());
